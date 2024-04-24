672afebb5e6dbcff97f5a2fb51c26505bed4ac5b
OofMani <theadityamani@gmail.com>
2024-04-24 17:45:57 +0800

    Import files

---
+++ app.js
@@ -0,0 +1,37 @@
+
+    setTimeout(() => {
+        document.getElementById('sign').style.opacity=1;
+        }, 5000);
+    
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        console.log(entry.target)
+        if (entry.isIntersecting) {
+            
+
+            entry.target.classList.add('show');
+            
+        }else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+
+
+// m_title = document.getElementById('m_title');
+// m_sec = document.getElementById('m_sec');
+// if(m_sec.classList.contains('show')){
+//     console.log("m_sec is show");
+//     m_title.classList.remove('mario_none');
+//     m_title.classList.add('mario');
+// }else{
+//     console.log("msecnot show");
+// }
+
+
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el))
